d51eb6305f6193e3df7693a9fb97af4ce1cbeeb7
Jose Luis Rodriguez <jose@Joses-Mac-mini.local>
2020-10-15 23:48:24 -0500

    Code replaced for starting the Todo App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Todo from './components/Todo';
 
 function App() {
-  const date = new Date();
-  const hours = date.getHours();
-  let timeOfDay = hours < 12 ? "morning" : hours >= 12 && hours <= 17 ? "afternoon" : "night";
-
-  const styles = {
-    color: (hours < 12 ? "yellow" : hours >= 12 && hours <= 17 ? "orange" : "black"),
-    fontSize: 20
-  };
-
   return (
     <div className="App">
-      <h1 style={styles}> Good {timeOfDay} !</h1>
+      <Todo/>
     </div>
   );
 }
