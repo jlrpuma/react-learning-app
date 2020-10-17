9127656dec38f2297b98cb891ede3c8794e677d0
Jose Luis Rodriguez <jose@Joses-Mac-mini.local>
2020-10-17 14:06:58 -0500

    Migration from functional component to class based component.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,23 +4,33 @@ import './Todo.css'
 
 import tasks from './../data/tasks.json'
 
-function Todo(props) {
-    // let taskComponents = tasks.map(task => (<TodoItem key={task.id} item={{message: task.message, status: task.status}}></TodoItem>));    
-    let taskComponents = tasks
-    .map(task => {
-        task.completed = task.status === "NOTDONE" ? false : true;
-        return task;
-    })
-    //.filter(task => task.deadline ? new Date(task.deadline) > new Date() && task.status === 'NOTDONE' : task)
-    .filter(task => task.deadline ? new Date(task.deadline) > new Date() : task)
-    .sort((task1, task2) => task1.deadline > task2.deadline)
-    .map(task => <TodoItem key={task.id} item={task} />);
-    
-    return (
-        <div className="todo-list">
-            {taskComponents}
-        </div>
-    );
+class Todo extends React.Component {
+
+    processInfo() {
+
+        // let taskComponents = tasks.map(task => (<TodoItem key={task.id} item={{message: task.message, status: task.status}}></TodoItem>));    
+
+        return tasks
+        .map(task => {
+            task.completed = task.status === "NOTDONE" ? false : true;
+            return task;
+        })
+        //.filter(task => task.deadline ? new Date(task.deadline) > new Date() && task.status === 'NOTDONE' : task)
+        .filter(task => task.deadline ? new Date(task.deadline) > new Date() : task)
+        .sort((task1, task2) => task1.deadline > task2.deadline)
+        .map(task => <TodoItem key={task.id} item={task} />);
+    }
+
+    render() {
+        /* this keyword is needed for access your own methods on the class */
+        let taskComponents = this.processInfo();
+        return (
+            <div className="todo-list">
+                {taskComponents}
+            </div>        
+        );
+    }
+
 }
 
 export default Todo;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,20 +2,21 @@ import React from 'react';
 import Todo from './Todo';
 import './TodoItem.css'
 
+class TodoItem extends React.Component {
 
-function TodoItem(props) {
+    render() {
+        let taskStyle = {
+            color: !this.props.item.status || this.props.item.status === 'NOTDONE' ? 'red' : 'blue'
+        }
 
-    let taskStyle = {
-        color: !props.item.status || props.item.status === 'NOTDONE' ? 'red' : 'blue'
+        return (
+            <div className="todo-item">
+                <input type="checkbox" checked={this.props.item.completed}/>
+                {/*the item.message property can be handled here (given the proprs of the component)*/}
+                <p style={taskStyle}> {this.props.item.message}</p>
+            </div>
+        );
     }
-
-    return (
-        <div className="todo-item">
-            <input type="checkbox" checked={props.item.completed}/>
-            {/*the item.message property can be handled here (given the proprs of the component)*/}
-            <p style={taskStyle}> {props.item.message}</p>
-        </div>
-    );
 }
 
 export default TodoItem;
