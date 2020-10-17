b7d5205251492042525f05dbe92f0e363114b865
Jose Luis Rodriguez <jose@Joses-Mac-mini.local>
2020-10-17 12:49:10 -0500

    Added some sorting logic based on deadline time.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,7 +6,12 @@ import tasks from './../data/tasks.json'
 
 function Todo(props) {
     // let taskComponents = tasks.map(task => (<TodoItem key={task.id} item={{message: task.message, status: task.status}}></TodoItem>));    
-    let taskComponents = tasks.filter(task => task.deadline ? new Date(task.deadline) > new Date() : task).map(task => <TodoItem key={task.id} item={{message: task.message, status: task.status}} />);
+    let taskComponents = 
+    tasks
+    .filter(task => task.deadline ? new Date(task.deadline) > new Date() && task.status === 'NOTDONE' : task)
+    .sort((task1, task2) => task1.deadline > task2.deadline)
+    .map(task => <TodoItem key={task.id} item={{message: task.message, status: task.status}} />)
+    
     
     return (
         <div>
