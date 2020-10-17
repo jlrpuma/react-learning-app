23857ba2ff7cb5b92973c880f533e20f8b42ca91
Jose Luis Rodriguez <jose@Joses-Mac-mini.local>
2020-10-17 13:58:42 -0500

    replacing a funcion based component to a class based component.
    As you can see the change is pretty easy to understand.
    
    The app class extends from a ReactComponent.
    And implements the render method so far...

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Todo from './components/Todo';
 
-function App() {
-  return (
-    <div>
-      <Todo/>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+      return (
+        <div>
+          <Todo/>
+        </div>
+      )
+  }
 }
 
 export default App;
