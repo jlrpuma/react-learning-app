45a882d157a37c77dd262edd541f719aeed734c1
Jose Luis Rodriguez <jose@Joses-Mac-mini.local>
2020-10-17 14:42:30 -0500

    Using the state to store the task that are comming from the json file.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,24 +6,28 @@ import tasks from './../data/tasks.json'
 
 class Todo extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {
+            tasks: tasks
+        };
+    }
+
+
     processInfo() {
-
-        // let taskComponents = tasks.map(task => (<TodoItem key={task.id} item={{message: task.message, status: task.status}}></TodoItem>));    
-
-        return tasks
+        this.state.tasks
         .map(task => {
             task.completed = task.status === "NOTDONE" ? false : true;
             return task;
         })
         //.filter(task => task.deadline ? new Date(task.deadline) > new Date() && task.status === 'NOTDONE' : task)
         .filter(task => task.deadline ? new Date(task.deadline) > new Date() : task)
-        .sort((task1, task2) => task1.deadline > task2.deadline)
-        .map(task => <TodoItem key={task.id} item={task} />);
+        .sort((task1, task2) => task1.deadline > task2.deadline);
     }
 
     render() {
         /* this keyword is needed for access your own methods on the class */
-        let taskComponents = this.processInfo();
+        let taskComponents =  this.state.tasks.map(task => <TodoItem key={task.id} item={task} />)
         return (
             <div className="todo-list">
                 {taskComponents}
