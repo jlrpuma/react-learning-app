2c8712a3c93c8fde4a4e157aab987d93e28f94ae
Jose Luis Rodriguez <jose@Joses-Mac-mini.local>
2020-10-17 14:20:54 -0500

    Introducing state....
    Small example of how to use it and what do you need in your class.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import './App.css';
 import Todo from './components/Todo';
 
 class App extends React.Component {
+
+  // initialize the class like on any other OOP language
+  constructor() {
+    super(); // go to the supperclass to initialize that class
+    // now we have an state on our component that can be touched on any place of the Component using this keyword
+    this.state = {
+      answer : "yes"
+    } 
+  }
+
   render() {
       return (
         <div>
-          <Todo/>
+          {/* <Todo/> */}
+          <h1> Is state important to know?</h1>
+          <p>{this.state.answer}</p>
         </div>
       )
   }
